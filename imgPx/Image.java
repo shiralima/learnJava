74c2ab767ea8ca49d7f1711eed41a4f58102820c
hilma <shiral@hilma.tech>
2023-10-09 18:36:35 +0300

    px img ex: in imgPx folder solve with class ;

---
+++ imgPx/Image.java
@@ -0,0 +1,9 @@
+package imgPx;
+
+public class Image {
+    public static void main(String[] args) {
+        ImagePixels img = new ImagePixels(10 , 's' , 'i');
+        img.DrewImage();
+    }
+
+}
---
+++ imgPx/imagePixels.java
@@ -0,0 +1,38 @@
+package imgPx;
+
+class ImagePixels {
+
+    private int size;
+    private char firstColor;
+    private char secondColor;
+    private char img[][];
+
+    /**
+     * This Class get size (n X n) size for the img and also get the color/symbol to
+     * drew the img by
+     */
+    public ImagePixels(int size, char firstColor, char secondColor) {
+        this.size = size;
+        this.firstColor = firstColor;
+        this.secondColor = secondColor;
+        this.img = new char[size][size];
+
+        boolean isCurrColorFirst = true;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                this.img[row][col] = isCurrColorFirst ? this.firstColor : this.secondColor;
+                isCurrColorFirst = !isCurrColorFirst;
+            }
+        }
+    }
+
+    public void DrewImage() {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                System.out.print(this.img[row][col]);
+            }
+            System.out.println();
+        }
+    }
+
+}
